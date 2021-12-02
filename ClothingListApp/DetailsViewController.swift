eb621fdad2c6a88c37e3d793848a1b8345b05c2e
Ömer <omeroguzcelikel@gmail.com>
2021-12-02 21:36:42 +0300

    detailsViewController
    image save done

--- ClothingListApp/DetailsViewController.swift
+++ ClothingListApp/DetailsViewController.swift
@@ -6,8 +6,8 @@
 //
 
 import UIKit
-
-class DetailsViewController: UIViewController {
+import CoreData
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var priceTextField: UITextField!
@@ -18,6 +18,27 @@ class DetailsViewController: UIViewController {
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+        
+        // user can interact with image (will click)
+        imageView.isUserInteractionEnabled = true
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseYourProduct))
+        imageView.addGestureRecognizer(imageGestureRecognizer)
+    }
+    
+    // will pick photo from gallary
+    @objc func chooseYourProduct(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    //function is for after pick media
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.editedImage] as? UIImage
+        //closses picker
+        self.dismiss(animated: true, completion: nil)
     }
     
     @objc func closeKeyboard(){
@@ -25,6 +46,32 @@ class DetailsViewController: UIViewController {
     }
     
     @IBAction func saveButtonClicked(_ sender: Any) {
+        // can find AppDelegate for save
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let clothes = NSEntityDescription.insertNewObject(forEntityName: "Clothes", into: context)
+        
+        clothes.setValue(nameTextField.text!, forKey: "name")
+        clothes.setValue(sizeTextField.text!, forKey: "size")
+        
+        if let price = Int(priceTextField.text!){
+            clothes.setValue(price, forKey: "price")
+        }
+        
+        //universal unique id
+        clothes.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        
+        clothes .setValue(data, forKey: "image")
+        do{
+            try context.save()
+            print("saved")
+        }catch{
+            print("there is a ERROR")
+        }
+        
     }
     
     
--- ClothingListApp/ViewController.swift
+++ ClothingListApp/ViewController.swift
@@ -6,23 +6,61 @@
 //
 
 import UIKit
+import CoreData
 
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+    }
+    
+    
+    func takeData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        // needs data taker
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Clothes")
+        
+        // ıf there is many datas
+        fetchRequest.returnsObjectsAsFaults = false
+        do{
+            // results are empty list NSManagedObkect uses when Core Date uses
+            let results = try context.fetch(fetchRequest)
+            for result in results as! [NSManagedObject]{
+                //gives NSManagedObject
+                //result.value(forKey: <#T##String#>)
+                let name = result.value(forkey: "name") as? String
+                print(name)
+            }
+        } catch{
+            print("there is a error")
+        }
         
     }
+    
+    
+    
     @objc func addButtonClicked(){
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Text"
+        return cell
+    }
 
 }
 
